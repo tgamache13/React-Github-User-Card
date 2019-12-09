55f7f664ccfafdf7fa9ede40f5a440db4d686579
Tara Gamache <tgamache.13@gmail.com>
2019-12-09 05:50:24 -0800

    added some styling started on contribution calendar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-
+import GitHubCalendar from 'react-github-calendar';
+import './styles.scss';
 
 class App extends React.Component {
   constructor() {
@@ -10,6 +11,15 @@ class App extends React.Component {
       followers: []
     }
   }
+  defaultTheme = {
+    background: 'transparent',
+    text:'white',
+    grade4: '#196127',
+    grade3: '#239a3b',
+    grade2: '#7bc96f',
+    grade1: '#c6e48b',
+    grade0: '#ebedf0'
+  };
   componentDidMount() {
     axios.get(`https://api.github.com/users/tgamache13`)
       .then(result => {
@@ -21,7 +31,7 @@ class App extends React.Component {
       .catch(error =>  {
         console.log('error', error)
       })
-
+    
       axios
       .get(`https://api.github.com/users/tgamache13/followers`)
         .then(result => {
@@ -32,23 +42,33 @@ class App extends React.Component {
           })
           .catch(error => {
             console.log('error', error)
-          }) 
+          })
+      
   }
   render() {
     return (
-      <div>
+      <div class="container">
         <img src={this.state.user.avatar_url} />
+      
         <h1>{this.state.user.name}</h1>
         <h3>{this.state.user.bio}</h3>
         <h3>{this.state.user.location}</h3>
         <h3>{this.state.user.login}</h3>
-        <h3>{this.state.user.url}</h3>
-        <div>
+        
+        <a href="https://api.github.com/users/{this.state.user.login}">{this.state.user.url}</a>
+        
+        {/* <div class='calendar'>
+        
+           <GitHubCalendar id='calendar' theme={this.defaultTheme} username={this.state.user.login}/>
+          <script>GitHubCalendar(".calendar", "tgamache13")</script>
+          
+        </div> */}
+      <div className='followers'>
           {this.state.followers.map((follower, index) => (
             <div key={index}>
             <img  src={follower.avatar_url} />
             <h2> {follower.login}</h2>
-            <h3>{follower.url}</h3>
+            {/* <h3>{follower.url}</h3> */}
           </div>
           ))}
             
